74bd678de292f458442edbb55dd5f9510925401c
Debaleena123 <149792472+Debaleena123@users.noreply.github.com>
2023-11-09 23:07:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,8 @@ streamlit.title('My name is Debaleena Biswas')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include 
-Fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+Fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Banana','Lemon'])
+Fruits_to_show = my_fruit_list.loc[Fruits_selected]
 streamlit.dataframe(my_fruit_list)
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
